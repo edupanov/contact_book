2b9311b85f546fbf3f965b33c9f0cf54fa44fa93
Vyacheslav <kleshachenok90@gmail.com>
2021-06-30 16:33:27 +0300

    create main form

---
+++ src/components/mainForm/MainForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from "./MainForm.module.scss"
+
+class MainForm extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default MainForm;
