eba5e3734ac100f2730666971950a97f98464f19
edupanov <edupanov@gmail.com>
2021-07-08 21:19:43 +0300

    небольшой фикс

--- src/app/components/searchUser/SearchPage.tsx
+++ src/app/components/searchUser/SearchPage.tsx
@@ -3,8 +3,6 @@ import {Button, FormControl, FormGroup, Grid, TextField} from "@material-ui/core
 import {useActions} from "../../store/hooks/useActions";
 import style from './searchUser.module.scss'
 
-type FormikErrorType = {}
-
 type TargetType = {
     name: string
     value: string
@@ -18,6 +16,9 @@ const SearchPanel: FC = () => {
 
     const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
         const target: TargetType = (event.target)
+        console.log(event.target.value)
+        const replaceStr = event.target.value.replace(/-/g, ' ').split(' ').reverse().join('.')
+        console.log(replaceStr)
 
         setSearch({
             ...search,
@@ -47,43 +48,85 @@ const SearchPanel: FC = () => {
                                     <TextField className={style.input}
                                                label="Имя"
                                                name={"name"}
-                                               onChange={changeHandler}
-                                    />
+                                               type="search"
+                                               onChange={changeHandler}/>
                                     <TextField className={style.input}
                                                label="Фамилия"
                                                name={"surname"}
+                                               type="search"
                                                onChange={changeHandler}
                                     />
                                     <TextField className={style.input}
                                                label="Отчество"
                                                name={"patronymic"}
+                                               type="search"
                                                onChange={changeHandler}
                                     />
                                     <TextField className={style.input}
                                                label="Дата рождения"
+                                               helperText="Дата рождения"
                                                name={"birthDate"}
+                                               type="date"
                                                onChange={changeHandler}
                                     />
                                     <TextField className={style.input}
                                                label="Пол"
                                                name={"gender"}
+                                               type="search"
                                                onChange={changeHandler}
                                     />
                                     <TextField className={style.input}
                                                label="Семейное положение"
                                                name={"family"}
+                                               type="search"
                                                onChange={changeHandler}
                                     />
                                     <TextField className={style.input}
                                                label="Гражданство"
                                                name={"nationality"}
+                                               type="search"
                                                onChange={changeHandler}
                                     />
-                                    <TextField className={style.input}
-                                               label="Адрес"
-                                               name={"address"}
-                                               onChange={changeHandler}
+                                    <div className={style.addressWrapper}>
+                                        <span className={style.address}>Адрес</span>
+                                        <br/><TextField className={style.input}
+                                                        label="Страна"
+                                                        name={"country"}
+                                                        type="search"
+                                                        onChange={changeHandler}
                                     />
+                                        <TextField className={style.input}
+                                                   label="Город"
+                                                   name={"city"}
+                                                   type="search"
+                                                   onChange={changeHandler}
+                                        />
+                                        <TextField className={style.input}
+                                                   label="Улица"
+                                                   name={"street"}
+                                                   type="search"
+                                                   onChange={changeHandler}
+                                        />
+                                        <TextField className={style.input}
+                                                   label="Номер дома"
+                                                   name={"building"}
+                                                   type="number"
+                                                   onChange={changeHandler}
+                                        />
+                                        <TextField className={style.input}
+                                                   label="Номер квартиры"
+                                                   name={"flat"}
+                                                   type="number"
+                                                   onChange={changeHandler}
+                                        />
+                                        <TextField className={style.input}
+                                                   label="Индекс"
+                                                   name={"zipCode"}
+                                                   type="number"
+
+                                                   onChange={changeHandler}
+                                        />
+                                    </div>
                                 </div>
                                 <div>
                                     <Button type={'submit'} variant={'contained'} color={'primary'}
