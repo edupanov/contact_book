130d635ce37d2cc18fbf98ff4f46987e967dc8e3
klesh <kleshchenok90@gmail.com>
2021-07-08 16:53:11 +0300

    Обновил получение контактов на бэке

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -3,6 +3,6 @@ const UserController = require('../controllers/userController')
 
 const router = express.Router()
 
-router.post('', UserController.getUsers)
+router.get('',  UserController.getUsers)
 
 module.exports = router
