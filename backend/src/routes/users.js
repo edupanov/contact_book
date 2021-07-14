66cf1c595e54a76c4c5baee853415fb26445af0a
klesh <kleshchenok90@gmail.com>
2021-07-14 16:19:17 +0300

    рефакторинг

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -4,5 +4,6 @@ const UserController = require('../controllers/userController')
 const router = express.Router()
 
 router.post('',  UserController.getUsers)
+router.post('/create',  UserController.createContact)
 
 module.exports = router
