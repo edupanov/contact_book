b46b81af8bbe7a70e89c78a2fdd0ad0a32c9842f
klesh <kleshchenok90@gmail.com>
2021-07-21 15:25:23 +0300

    Добавил метод для удаления всех контактов

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -214,12 +214,35 @@ module.exports = {
 
         await User.deleteMany({_id: deleted})
             .then(count => {
-               res.status(200).json({count})
+               res.status(200).json({
+                   code: 200,
+                   isSuccess: true,
+                   message: 'Contacts deleted successfully!',
+                   count
+               })
             })
+    },
 
-        // deleted.map(async contactId => {
-        //     const res = await User.deleteOne({_id: contactId})
-        // })
+    deleteAllContacts: async (req, res, next) => {
+
+        await User.find(async documents => {
+            const contacts = documents
+            const contactsCount = await User.countDocuments()
+
+            contacts.map(contact => contact._id)
+
+            await User.deleteMany({_id: contacts})
+                .then(count => {
+                    if (count === contactsCount) {
+                        res.status(200).json({
+                            code: 200,
+                            isSuccess: true,
+                            message: 'Contacts deleted successfully!',
+                            count
+                        })
+                    }
+                })
+        })
     },
 
     setContacts: async (req, res, next) => {
