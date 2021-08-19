04de56dfbe599c4b44a61a9a37f3112eb6a4e003
Vyacheslav <kleshachenok90@gmail.com>
2021-08-19 09:48:21 +0300

    fix

--- src/app/components/pages/editPage/phone/AddPhoneForm.tsx
+++ src/app/components/pages/editPage/phone/AddPhoneForm.tsx
@@ -11,13 +11,18 @@ interface AddPhoneFormInterface {
 
 export const AddPhoneForm = (props: AddPhoneFormInterface) => {
 
+    const [errors, setError] = useState({
+        countryCode: '',
+        operatorID: '',
+        phoneNumber: '',
+    })
+
     const classes = useStylesAddPhone();
     let {setOpen, contact} = props
 
     const {addPhone} = useActions()
     const [phone, setPhone] = useState({} as PhoneInterface)
     const phoneId = contact.phones ? contact.phones.length + 1 : 1
-
 
     const changePhoneInfoHandler = (event: ChangeEvent<HTMLInputElement>) => {
         const {name, value} = event.target
@@ -32,6 +37,35 @@ export const AddPhoneForm = (props: AddPhoneFormInterface) => {
         setOpen(false)
     }
 
+    const validation = (event: React.FocusEvent<HTMLInputElement>) => {
+        const target = event.target
+        const regexCountryCode = /^(\+?\d{1,3}|\d{1,4})$/;
+        const regexOperatorID = /^[0-9]{2}$/;
+        const regexPhoneNumber = /^[0-9]{7}$/;
+        if (target.name === 'countryCode') {
+            if (!regexCountryCode.test(target.value)) {
+                setError({...errors, countryCode: 'Поле запонено неверно'})
+            } else if (regexCountryCode.test(target.value)) {
+                setError({...errors, countryCode: ''})
+            }
+        }
+        if (target.name === 'operatorID') {
+            if (!regexOperatorID.test(target.value)) {
+                setError({...errors, operatorID: 'Поле запонено неверно'})
+            } else if (regexOperatorID.test(target.value)) {
+                setError({...errors, operatorID: ''})
+            }
+        }
+        if (target.name === 'phoneNumber') {
+            if (!regexPhoneNumber.test(target.value)) {
+                setError({...errors, phoneNumber: 'Поле запонено неверно'})
+            } else if (regexPhoneNumber.test(target.value)) {
+                setError({...errors, phoneNumber: ''})
+            }
+        }
+        return errors
+    }
+
     return (
         <div>
             <Grid container justifyContent="center">
@@ -41,24 +75,38 @@ export const AddPhoneForm = (props: AddPhoneFormInterface) => {
                             <FormGroup>
                                 <div className={classes.wrapperInput}>
                                     <TextField className={classes.input}
+                                               title={'Код в формате +ХХХ'}
                                                label="Код станы"
                                                name={"countryCode"}
                                                type="search"
+                                               onBlur={validation}
                                                onChange={changePhoneInfoHandler}
-
                                     />
+                                    {errors.countryCode ?
+                                        <div className={classes.error}>{errors.countryCode}</div> : null}
+
                                     <TextField className={classes.input}
+                                               title={'Код в формате ХХ'}
                                                label="Код оператора"
                                                name={"operatorID"}
                                                type="search"
+                                               onBlur={validation}
                                                onChange={changePhoneInfoHandler}
                                     />
+                                    {errors.operatorID ?
+                                        <div className={classes.error}>{errors.operatorID}</div> : null}
+
                                     <TextField className={classes.input}
+                                               title={'Номера в формате ХХХХХХХ'}
                                                label="Телефонный номер"
                                                name={"phoneNumber"}
                                                type="search"
+                                               onBlur={validation}
                                                onChange={changePhoneInfoHandler}
                                     />
+                                    {errors.phoneNumber ?
+                                        <div className={classes.error}>{errors.phoneNumber}</div> : null}
+
                                     <TextField className={classes.input}
                                                label="Тип"
                                                name={"phoneType"}
